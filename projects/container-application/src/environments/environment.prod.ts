804c2a96fa2bddddaa89e879f212f83c3f818005
ArunVashishtha <arunvashishtha38@gmail.com>
2025-01-13 21:48:06 +0530

    remove back slassh

--- projects/container-application/src/environments/environment.prod.ts
+++ projects/container-application/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  insuranceDetailsMFE: 'https://insurance-details-3142877.netlify.app/',
-  premiumPaymentsMFE: 'https://premium-payments-3142877.netlify.app/',
+  insuranceDetailsMFE: 'https://insurance-details-3142877.netlify.app',
+  premiumPaymentsMFE: 'https://premium-payments-3142877.netlify.app',
 };
